77f63d95f98ed2bc8deaf162efe494adc1699650
Guo Yunhe <i@guoyunhe.me>
2023-08-20 20:41:59 +0800

    mantine

--- src/data/design-systems/index.ts
+++ src/data/design-systems/index.ts
@@ -5,6 +5,7 @@ import blueprint5 from './blueprint5';
 import bootstrap5 from './bootstrap5';
 import chakraUI from './chakraUI';
 import fluentUI from './fluentUI';
+import mantine from './mantine';
 import materialUI5 from './materialUI5';
 import radixUI from './radixUI';
 
@@ -13,6 +14,7 @@ const designSystemListData: DesignSystem[] = [
   antDesign5,
   bootstrap5,
   chakraUI,
+  mantine,
   blueprint5,
   fluentUI,
   radixUI,
